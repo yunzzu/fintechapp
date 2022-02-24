87c56f01c4a13913bbed0093dfff236cdeefd0d9
yunzzu <whdsbswn@gmail.com>
2022-02-25 01:58:16 +0900

    first js

---
+++ test.js
@@ -0,0 +1,35 @@
+//var name = "윤주";
+//console.log(name);
+
+
+//function test(a,b){
+//    return a+b;
+//}
+//console.log(test(2,3));
+
+//const plus = (a,b) => {
+//    return a+b;
+//}
+//console.log(plus(9,8));
+
+
+//let names = ["윤주", "현준", "지현"];
+//for(let i=0;i<names.length;i++){
+    //let res = names[i];
+//    console.log(names[i]);
+//}
+//names.map((name) => {      //배열 메소드 이용 
+//    console.error(name);
+//})
+
+//map함수 이용해서 if문
+//names.map((name) => {
+//    if(name==="윤주"){
+//        console.log("윤주 있음");
+//    }
+//})
+
+
+//let name = "윤주";
+//let age = 23;
+//console.log(`${name}의 나이는 ${age}입니다`);
