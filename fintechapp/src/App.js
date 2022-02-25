ad0b1646d775bb17fd02e6095274417a297875ee
yunzzu <whdsbswn@gmail.com>
2022-02-26 01:34:03 +0900

    axiostest

--- fintechapp/src/App.js
+++ fintechapp/src/App.js
@@ -4,12 +4,16 @@ import ListComp from './components/ListComp';
 import Header from './components/Header';
 import styled from 'styled-components';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AxiosTest from './pages/AxiosTest';
 
 function App() {
       return(
         <BrowserRouter>
           <Routes>
             <Route path="/test/" element={<ListComp />}></Route>
+          </Routes>
+          <Routes>
+            <Route path="/axiosTest" element={<AxiosTest />}></Route>
           </Routes>
         </BrowserRouter>
       );
---
+++ fintechapp/src/pages/AxiosTest.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const AxiosTest = () => {
+  return (
+    <div>
+        <Header title="Axios Test" />
+        <button>데이터 요청</button>
+    </div>
+  )
+}
+
+export default AxiosTest
