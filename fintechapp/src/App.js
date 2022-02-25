42cf0a4768910168188d1f3c8cd20516a40eca89
yunzzu <whdsbswn@gmail.com>
2022-02-26 01:07:41 +0900

    install react-router-dom

--- fintechapp/src/App.js
+++ fintechapp/src/App.js
@@ -3,39 +3,16 @@ import { useState } from "react"; //state hook을 import
 import ListComp from './components/ListComp';
 import Header from './components/Header';
 import styled from 'styled-components';
-
-const WholeBlock = styled.div`
-    margin: 20px;
-    text-align: center;
-`;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
-  let username = "미입력";
-  let [usernameWithState, setUsernameWithState] = useState("초기값");
-  //[a,b] -> [데이터를 불러올 변수, a데이터를 바꿀 수 있는 메소드가 숨어있는? 상태]
-
-  const handleInputChange = (e) => {
-    //console.log(e.target.value); //입력될 데이터
-    //username = e.target.value;   //받아온 값을 username에 넣어줌
-    setUsernameWithState(e.target.value);
-    console.log(username);
+      return(
+        <BrowserRouter>
+          <Routes>
+            <Route path="/test/" element={<ListComp />}></Route>
+          </Routes>
+        </BrowserRouter>
+      );
   };
-
-  return (
-    <div>
-      <Header title="settings" />
-      <br></br>
-      <WholeBlock>
-          <div>입력: <input onChange={handleInputChange}></input></div>
-          {/* 주석 처리: ctrl+/ */}
-          {/* <Profile username="윤주" grade="4"></Profile>  */}
-          <h4>{usernameWithState}</h4>
-          <ListComp></ListComp>
-      </WholeBlock>
-    </div>
-  );
-
-}
 
 export default App;
